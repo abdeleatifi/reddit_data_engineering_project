4c7248b2b528bc5a6b4b8c02d9d7d0c58ff89db7
Abdellatif Atifi <abdele.1999maroc@gmail.com>
2023-11-03 18:03:45 +0100

    the last

--- dags/reddit_dag.py
+++ dags/reddit_dag.py
@@ -1,7 +1,8 @@
-from airflow import DAG
-from datetime import datetime
 import os
 import sys
+from datetime import datetime
+
+from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -9,11 +10,9 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from pipelines.aws_s3_pipeline import upload_s3_pipeline
 from pipelines.reddit_pipeline import reddit_pipeline
 
-
-
 default_args = {
-    'owner': 'abdele atifi',
-    'start_date': datetime(2023, 10, 26)
+    'owner': 'Yusuf Ganiyu',
+    'start_date': datetime(2023, 10, 22)
 }
 
 file_postfix = datetime.now().strftime("%Y%m%d")
@@ -38,3 +37,12 @@ extract = PythonOperator(
     },
     dag=dag
 )
+
+# upload to s3
+upload_s3 = PythonOperator(
+    task_id='s3_upload',
+    python_callable=upload_s3_pipeline,
+    dag=dag
+)
+
+extract >> upload_s3
